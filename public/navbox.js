c384d89449da8d8ac6125d3aa56afb341a5c9364
Col <colbyackerman@gmail.com>
2021-07-27 17:36:03 -0500

    add click event listeners to navbox buttons

---
+++ public/navbox.js
@@ -0,0 +1,41 @@
+// button handles
+const overview_button = document.getElementById('overview_button');
+const howitwasbuilt_button = document.getElementById('howitwasbuilt_button');
+const chipping_button = document.getElementById('chipping_button');
+const seeding_button = document.getElementById('seeding_button');
+const germination_button = document.getElementById('germination_button');
+const plantselection_button = document.getElementById('plantselection_button');
+const pollination_button = document.getElementById('pollination_button');
+const tour360_button = document.getElementById('tour360_button');
+
+overview_button.addEventListener('click', (event) => {
+    console.log ("clicked 'OVERVIEW' button")
+})
+
+howitwasbuilt_button.addEventListener('click', (event) => {
+    console.log ("clicked 'HOW IT WAS BUILT' button")
+})
+
+chipping_button.addEventListener('click', (event) => {
+    console.log ("clicked 'CHIPPING' button")
+})
+
+seeding_button.addEventListener('click', (event) => {
+    console.log ("clicked 'SEEDING' button")
+})
+
+germination_button.addEventListener('click', (event) => {
+    console.log ("clicked 'GERMINATION' button")
+})
+
+plantselection_button.addEventListener('click', (event) => {
+    console.log ("clicked 'PLANT SELECTION' button")
+})
+
+pollination_button.addEventListener('click', (event) => {
+    console.log ("clicked 'POLLINATION' button")
+})
+
+tour360_button.addEventListener('click', (event) => {
+    console.log ("clicked '360 TOUR' button")
+})
