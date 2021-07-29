77c9602aa6f8987374eb78b28aa888aca5c0802d
Col <colbyackerman@gmail.com>
2021-07-29 13:51:52 -0500

    add logo button + draw birds eye view to canvas

--- public/navbox.js
+++ public/navbox.js
@@ -10,51 +10,59 @@ const plantselection_button = document.getElementById('plantselection_button');
 const pollination_button = document.getElementById('pollination_button');
 const tour360_button = document.getElementById('tour360_button');
 
+const logo_button = document.getElementById('logo_button');
+
 
 // event listeners
 overview_button.addEventListener('click', () => {
     console.log ("clicked 'OVERVIEW' button");
     showVideoPlayer();
-})
+});
 
 
 howitwasbuilt_button.addEventListener('click', () => {
     console.log ("clicked 'HOW IT WAS BUILT' button");
     showVideoPlayer();
-})
+});
 
 
 chipping_button.addEventListener('click', () => {
     console.log ("clicked 'CHIPPING' button");
     showVideoPlayer();
-})
+});
 
 
 seeding_button.addEventListener('click', () => {
     console.log ("clicked 'SEEDING' button");
     showVideoPlayer();
-})
+});
 
 
 germination_button.addEventListener('click', () => {
     console.log ("clicked 'GERMINATION' button");
     showVideoPlayer();
-})
+});
 
 
 plantselection_button.addEventListener('click', () => {
     console.log ("clicked 'PLANT SELECTION' button");
     showVideoPlayer();
-})
+});
 
 
 pollination_button.addEventListener('click', () => {
     console.log ("clicked 'POLLINATION' button");
     showVideoPlayer();
-})
+});
 
 
 tour360_button.addEventListener('click', () => {
     console.log ("clicked '360 TOUR' button");
     showVideoPlayer();
-})
+});
+
+
+logo_button.addEventListener ('click', () => {
+    console.log ("clicked 'LOGO' button");
+    window.open("https://www.bayer.com/en/");
+});
--- public/vtour.js
+++ public/vtour.js
@@ -1,11 +1,22 @@
-import * as THREE from 'https://cdn.skypack.dev/three@latest';
-import * as Config from './config.js';
+import { WIDTH, HEIGHT } from "./config.js";
 
+const canvas = document.getElementById ('c');
+const ctx = canvas.getContext('2d');
 
-function main() {
-  const canvas = document.querySelector('#c');
-  const renderer = new THREE.WebGLRenderer(canvas);
+const birds_eye = new Image ();
+birds_eye.src = './images/marana_birds_eye.png';
 
+function home() {
+
+  ctx.canvas.width  = window.innerWidth;
+  ctx.canvas.height = window.innerHeight;
+
+  // ctx.fillStyle = 'black';
+  // ctx.fillRect(0, 0, WIDTH, HEIGHT);
+
+  ctx.drawImage (birds_eye, -50, -50, WIDTH + 50, HEIGHT + 50);
+
+  requestAnimationFrame(home);
 }
 
-main();
+home();
