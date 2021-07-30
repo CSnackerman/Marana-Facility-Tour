e46659155cec3d5c261e4a4df13231574002d788
Col <colbyackerman@gmail.com>
2021-07-30 01:03:56 -0500

    responsive navbox

--- public/navbox.js
+++ public/navbox.js
@@ -1,6 +1,17 @@
 import { showVideoPlayer } from "./videoplayer.js";
+import { WIDTH, HEIGHT } from "./config.js";
+import { R_MODE } from "./config.js";
+
+// navbox container
+const navbox_container = document.getElementById ('navbox_container');
+
+
+// topper
+const navbox_topper = document.getElementById('navbox_topper');
 
 // button handles
+const navbox_buttons = document.querySelectorAll('.navbox-button');
+
 const overview_button = document.getElementById('overview_button');
 const howitwasbuilt_button = document.getElementById('howitwasbuilt_button');
 const chipping_button = document.getElementById('chipping_button');
@@ -11,6 +22,36 @@ const pollination_button = document.getElementById('pollination_button');
 const tour360_button = document.getElementById('tour360_button');
 
 const logo_button = document.getElementById('logo_button');
+
+
+// initialize
+function initNavbox() {
+    
+    if (R_MODE === 'mobile') {
+
+        let m_width = '115px';
+
+        navbox_container.style.left = 0;
+
+        navbox_topper.style.width = m_width;
+        
+        navbox_buttons.forEach ( (button) => {
+            button.style.width = m_width;
+            button.style.fontSize = '7pt';
+            button.style.padding = '4px';
+            // console.log(button);
+        })
+
+        logo_button.style.right = 0;
+        logo_button.style.width = '40%';
+        logo_button.style.height = '5%';
+        logo_button.style.margin = 0;
+        logo_button.style.marginRight = '-25px';
+        console.log (logo_button);
+    }
+}
+
+initNavbox();
 
 
 // event listeners
