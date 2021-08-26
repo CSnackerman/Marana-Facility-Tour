d268e97d172be5f2c59bfda16d1b1c4938826cb6
C <colbyackerman@gmail.com>
2021-08-26 16:28:11 -0500

    add resize event listener for tour

--- public/tour.js
+++ public/tour.js
@@ -1,15 +1,19 @@
 import * as THREE from 'https://cdn.skypack.dev/three@latest';
 import { tourToggle } from './config.js';
+import { HEIGHT, WIDTH } from './config.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.domElement.style.zIndex = 5;
-renderer.domElement.style.position = 'fixed';
-console.log (renderer)
-document.body.appendChild( renderer.domElement );
+
+const canvas = renderer.domElement;
+canvas.style.zIndex = 5;
+canvas.style.position = 'fixed';
+console.log (canvas.style.width, canvas.style.height);
+
+document.body.appendChild( canvas );
 
 
 const geometry = new THREE.BoxGeometry();
@@ -35,5 +39,11 @@ const runTour = function () {
     
     requestAnimationFrame( runTour );
 };
+
+window.addEventListener ('resize', () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize (window.innerWidth, window.innerHeight);
+}, false);
 
 export { runTour };
