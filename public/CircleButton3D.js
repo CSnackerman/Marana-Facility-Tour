9c6e1023d2922acd4b67a7f3e91d257382105b3f
Colby <colbyackerman@gmail.com>
2021-08-29 00:07:54 -0500

    hover functionality to 3d circle button

--- public/CircleButton3D.js
+++ public/CircleButton3D.js
@@ -23,36 +23,42 @@ class CircleButton3D {
 
         // set the textures based on name
         switch (name) {
+
             case 'one': 
-                this.textTexure = CircleButton3D.textTextures.one;
+                this.textTexture = CircleButton3D.textTextures.one;
+                this.hoverTexture = CircleButton3D.hoverTextures.one;
                 break;
 
             case 'two': 
-                this.textTexure = CircleButton3D.textTextures.two;
+                this.textTexture = CircleButton3D.textTextures.two;
+                this.hoverTexture = CircleButton3D.hoverTextures.two;
                 break;
 
             case 'three': 
-                this.textTexure = CircleButton3D.textTextures.three;
+                this.textTexture = CircleButton3D.textTextures.three;
+                this.hoverTexture = CircleButton3D.hoverTextures.three;
                 break;
 
             case 'four': 
-                this.textTexure = CircleButton3D.textTextures.four;
+                this.textTexture = CircleButton3D.textTextures.four;
+                this.hoverTexture = CircleButton3D.hoverTextures.four;
                 break;
 
             case 'five': 
-                this.textTexure = CircleButton3D.textTextures.five;
+                this.textTexture = CircleButton3D.textTextures.five;
+                this.hoverTexture = CircleButton3D.hoverTextures.five;
                 break;
 
             case 'journey': 
-                this.textTexure = CircleButton3D.textTextures.journey;
+                this.textTexture = CircleButton3D.textTextures.journey;
+                this.hoverTexture = CircleButton3D.hoverTextures.journey;
                 break;
 
             default:
                 console.error ('invalid name for CircleButton3D ->', name);
-
         }
         this.textMaterial = new THREE.MeshBasicMaterial ({ 
-            map: this.textTexure, 
+            map: this.textTexture, 
             side: THREE.DoubleSide,
             transparent: true,
             opacity: 1
@@ -65,8 +71,24 @@ class CircleButton3D {
         this.textMesh.position.set (x - 0.01, y, z);
         this.textMesh.rotation.set (rotx, roty, rotz);
 
+
+        
         // hover stuff
+        this.hoverInterval = null;
         this.isHovered = false;
+        this.hoverMaterial = new THREE.MeshBasicMaterial ({ 
+            map: this.hoverTexture, 
+            side: THREE.DoubleSide,
+            transparent: true,
+            opacity: 1
+        });
+        this.hoverGeometry = new THREE.PlaneGeometry (1, 1);
+        this.hoverMesh = new THREE.Mesh (this.hoverGeometry, this.hoverMaterial);
+
+        // position and rotate hover plane
+        this.hoverMesh.position.set (x - 0.02, y, z);
+        this.hoverMesh.rotation.set (rotx, roty, rotz);
+        this.hoverScale = 0;
 
         console.log (this);
 
@@ -75,6 +97,7 @@ class CircleButton3D {
     static allInited = false;
 
     static textureLoader = new THREE.TextureLoader();
+
     static texture = CircleButton3D.textureLoader.load ('./images/videoicon-anim-scaled.png');
 
     static textTextures = {
@@ -95,8 +118,10 @@ class CircleButton3D {
         journey: CircleButton3D.textureLoader.load ('images/hover-360conveyortour.png')
     }
 
+    // animation stuff
     static animation_delay = 77; // ms
     static frame = 0;
+    static growRate = 17; // ms
 
     static initStaticTexture() {
         if (!CircleButton3D.allInited) {
@@ -113,6 +138,7 @@ class CircleButton3D {
     addToScene(scene) {
         scene.add (this.mesh);
         scene.add (this.textMesh);
+        scene.add (this.hoverMesh);
     }
     
     animate () {
@@ -130,20 +156,44 @@ class CircleButton3D {
 
                 // set the offset
                 this.mesh.material.map.offset.set (73.1428571429 * col, 1024 - 73.1428571429 * row);
-
-                // console.log (row, col)
-                // console.log (this.mesh.material.map.offset)
-
             }
         }, CircleButton3D.animation_delay);
     }
 
     onHover() {
-        console.log ('hi')
+
+        if (this.isHovered) {
+            return;
+        }
+
+        this.hoverMesh.visible = true;
+
+        this.hoverInterval = setInterval ( () => {
+
+        if (this.hoverScale < 0.1) {
+            this.hoverScale += 0.01;
+            let s = this.hoverScale;
+            this.hoverMesh.scale.set(s,s,s);
+        }
+
+        }, CircleButton3D.growRate)
+
+        this.isHovered = true;
+
     }
 
     noHover() {
+        this.isHovered = false;
+        this.hoverMesh.scale.set (0.01, 0.01, 0.01);
+        this.hoverScale = 0;
+        this.hoverMesh.visible = false;
         
+        if (this.hoverInterval === null) {
+            return;
+        }
+
+        clearInterval (this.hoverInterval);
+        this.hoverInterval = null;
     }
     
 }
